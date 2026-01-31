90176b2cf93b68aaa6b8e4f3a989c4e2cdf059db
Emmanuel Hamani <emmanuel.s.hamani@gmail.com>
2026-01-31 16:33:09 +0000

    Database models

---
+++ src/models.py
@@ -0,0 +1,49 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100), unique=True, nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+    role = db.Column(db.String(100), nullable=False)
+
+    transactions = db.relationship('Transaction', backref='user', lazy=True)
+
+class Budget(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    year = db.Column(db.String(4), default='2025')
+    total_fund = db.Column(db.Float, default=0.0)
+    remaining_fund = db.Column(db.Float, default=0.0)
+
+    transactions = db.relationship('Transaction', backref='budget', lazy=True)
+
+class Asset(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    location = db.Column(db.String(100), default='Storage')
+    status = db.Column(db.String(100), default='Fine')
+
+class Transaction(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    cost = db.Column(db.Float, nullable=False) # amount
+    note = db.Column(db.String(200), nullable=False)
+    timestamp = db.Column(db.DateTime, nullable=False)
+    category = db.Column(db.String(100), nullable=False)
+    author = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=True)
+    
+    budget_id = db.Column(db.Integer, db.ForeignKey('budget.id'), nullable=False)
+    document_id = db.Column(db.Integer, db.ForeignKey('document.id'), nullable=True) # receipt
+    event_id = db.Column(db.Integer, db.ForeignKey('event.id'), nullable=True)
+
+class Document(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    filename = db.Column(db.String(200), nullable=False)
+    upload_date = db.Column(db.DateTime, nullable=False)
+
+class Event(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.String(300), nullable=True)
+    date = db.Column(db.DateTime, nullable=False)
+    document_id = db.Column(db.Integer, db.ForeignKey('document.id'), nullable=True)
