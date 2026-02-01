e8ae302007a1dd93fa44abf733256e00df1f14e2
Emmanuel Hamani <emmanuel.s.hamani@gmail.com>
2026-02-01 19:08:57 +0000

    Fixed incorrect credentials flash

--- src/auth.py
+++ src/auth.py
@@ -1,4 +1,12 @@
-
+from models import User
 
 def validateUser(username, password):
-    return True # TODO: Implement actual validation logic
+    print(f"Validating user: {username}")
+    # 1. Search for the user by username
+    user = User.query.filter_by(username=username).first()
+
+    # 2. If user exists AND password matches
+    if user and user.password == password:
+        return user
+    # 3. Otherwise, return None
+    return None
